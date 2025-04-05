d806a4238664b06a2a2c7897d6a2108ef60abea0
ratiebareeng <bareengbiz@gmail.com>
2025-04-05 23:47:00 +0200

    Export package files

--- lib/flutter_navigation.dart
+++ lib/flutter_navigation.dart
@@ -1,5 +1,11 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library;
+
+// Navigation service
+export 'src/navigation_service.dart';
+export 'src/transitions/fade_route.dart';
+export 'src/transitions/scale_route.dart';
+export 'src/transitions/slide_down_route.dart';
+export 'src/transitions/slide_left_route.dart';
+// Export transitions
+export 'src/transitions/slide_right_route.dart';
+export 'src/transitions/slide_up_route.dart';
